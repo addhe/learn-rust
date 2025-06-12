0393f5c8fc783cfaccaabf6cea51912f7561b263
Addhe Warman ( Awan ) <addhe.warman@gmail.com>
2025-06-12 18:50:14 +0700

    belajar minggu pertama rust

---
+++ halo_cargo/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, Awan! You're now ready to start your Rust journey!");
+    println!("This is your first Rust program.");
+    println!("Keep coding and have fun with Rust!");
+    println!("Happy coding!");
+}
